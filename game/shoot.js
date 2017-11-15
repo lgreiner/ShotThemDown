e08fc1cace307af554da54fa1dfde7cad5af6449
lgreiner <loic.greiner@epita.fr>
2017-11-15 22:58:45 +0100

    add collision bullet
    bullet collision

--- game/shoot.js
+++ game/shoot.js
@@ -44,7 +44,15 @@ function bullet_collision()
     //collision between bullet and walls
     for (var i = 0; i < player1.bullets.length; i++)
     {
-        if (Math.abs(player1.bullets[i].position.x) >= WIDTH / 2 ||
+        if (player1.bullets[i].position.x >= enemy.graphic.position.x - 10 &&
+            player1.bullets[i].position.x <= enemy.graphic.position.x + 10 &&
+            player1.bullets[i].position.y >= enemy.graphic.position.y - 10 &&
+            player1.bullets[i].position.y <=  enemy.graphic.position.y + 10)
+        {
+            scene.remove(player1.bullets[i]);
+            player1.bullets.splice(i, 1);
+            i--;
+        } else if (Math.abs(player1.bullets[i].position.x) >= WIDTH / 2 ||
             Math.abs(player1.bullets[i].position.y) >= HEIGHT / 2)
         {
             scene.remove(player1.bullets[i]);
